ebc0dffcbbf9b11fc2eeae7f41cb3f9cd7c8a206
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-04-23 17:31:18 +0100

    feat: migrate interim Patent DB to disk

--- travisip/fetcher/admin.py
+++ travisip/fetcher/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Patent
 
-# Register your models here.
+@admin.register(Patent)
+class PatentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'patent_date', 'short_patent_data')
+    search_fields = ('patent_data',)
+    list_filter = ('patent_date',)
+
+    def short_patent_data(self, obj):
+        return (obj.patent_data[:50] + '...') if len(obj.patent_data) > 50 else obj.patent_data
+    short_patent_data.short_description = 'Patent Data'
