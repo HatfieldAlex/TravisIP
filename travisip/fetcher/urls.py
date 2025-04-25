48defd22d2f050105e031913c8a6f0d59d034619
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-04-25 15:35:22 +0100

    feat: improve webpage UX

--- travisip/fetcher/urls.py
+++ travisip/fetcher/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import patent_search
 
 urlpatterns = [
-    path('patents/', views.patent_search, name='patent_search'),
+    path('patents/', patent_search, name='patent_search'),
 ]
--- travisip/fetcher/views.py
+++ travisip/fetcher/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import DateRangeForm
 from .models import Patent
 
 def patent_search(request):
-    patents = None
-    form = DateRangeForm()
-
     if request.method == 'POST':
         form = DateRangeForm(request.POST)
         if form.is_valid():
             start_date = form.cleaned_data['start_date']
             end_date = form.cleaned_data['end_date']
-            patents = Patent.objects.filter(patent_date__range=(start_date, end_date))
-    # else:
-    #     form = DateRangeForm()
+            # Save dates to session for a redirect
+            request.session['start_date'] = str(start_date)
+            request.session['end_date'] = str(end_date)
+            return redirect('patent_search')  # Use your URL name here
+    else:
+        form = DateRangeForm()
 
-    return render(request, 'fetcher/patent_search.html', {'form': form, 'patents': patents})
+    # On GET, check if we just redirected here
+    patents = None
+    start = request.session.pop('start_date', None)
+    end = request.session.pop('end_date', None)
+    if start and end:
+        patents = Patent.objects.filter(patent_date__range=(start, end))
+
+    return render(request, 'fetcher/patent_search.html', {
+        'form': form,
+        'patents': patents
+    })
